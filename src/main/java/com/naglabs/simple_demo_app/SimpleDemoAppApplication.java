0fa611c24a55753769c6388e062fd7104af2bc33
nagapplications <nagapplications@gmail.com>
2026-05-10 17:41:21 +0100

    fist commit

---
+++ src/main/java/com/naglabs/simple_demo_app/SimpleDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.naglabs.simple_demo_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naglabs/simple_demo_app/SimpleDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naglabs.simple_demo_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
